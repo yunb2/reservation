e6fd4ee83acc2fad2fb524be6ea33f20efdfae22
유인근 <keun0390@miridih.com>
2021-02-14 22:01:47 +0900

    feat: add relation code with member and shop

--- src/main/java/me/minho/reservation/member/web/dto/MemberSaveRequest.java
+++ src/main/java/me/minho/reservation/member/web/dto/MemberSaveRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.minho.reservation.member.domain.Member;
 import me.minho.reservation.member.domain.MemberType;
+import me.minho.reservation.reservation.domain.Shop;
 import me.minho.reservation.reservation.web.dto.ShopSaveRequest;
 
 @Getter
@@ -20,16 +21,21 @@ public class MemberSaveRequest {
     private ShopSaveRequest shopSaveRequest;
 
     public Member toMember() {
-        if (memberType == MemberType.ADMIN && shopSaveRequest == null) {
-            throw new IllegalArgumentException("관리자는 샵 정보가 필요합니다");
-        }
-
-        return Member.builder()
+        Member member = Member.builder()
                 .email(email)
                 .password(password)
                 .name(name)
                 .memberType(memberType)
-                .shop(memberType == MemberType.ADMIN ? shopSaveRequest.toShop() : null)
                 .build();
+
+        if (member.isAdmin() && shopSaveRequest == null) {
+            throw new IllegalArgumentException("관리자는 샵 정보가 필요합니다");
+        }
+
+        if (member.isAdmin()) {
+            Shop shop = shopSaveRequest.toShop(member);
+            member.setShop(shop);
+        }
+        return member;
     }
 }
