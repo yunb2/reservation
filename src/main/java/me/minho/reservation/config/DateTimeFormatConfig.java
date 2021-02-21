765aea04e941d6938bd86aa77ce22d1ab8ffc679
채윤병 <ybchae0801@naver.com>
2021-02-21 22:33:22 +0900

    DateTimeFormatConfig 추가

---
+++ src/main/java/me/minho/reservation/config/DateTimeFormatConfig.java
@@ -0,0 +1,20 @@
+package me.minho.reservation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class DateTimeFormatConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setDateFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        registrar.registerFormatters(registry);
+    }
+}
